cbdec5d863d6f46c35c63ece7b3be73df852f8f1
igsr5 <i@gsr5.dev>
2022-02-01 16:57:32 +0900

    resize

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sandbox/bimg-options
 
 go 1.17
+
+require github.com/h2non/bimg v1.1.6
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/h2non/bimg"
+)
 
 func main() {
-	fmt.Println("Hello")
+	buffer, err := bimg.Read("img/src.png")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	src := bimg.NewImage(buffer)
+
+	resize(src)
+}
+
+func resize(src *bimg.Image) {
+	buf, err := src.Resize(800, 600)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	size, err := src.Size()
+	if size.Width != 800 && size.Height != 600 {
+		fmt.Println("The image size is invalid")
+	}
+
+	bimg.Write("src.png", buf)
 }
